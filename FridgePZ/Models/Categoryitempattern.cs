d576db5a73380ed8129559ccfdd3c1f5ba96540c
Radek Jachimek <225951@student.pwr.edu.pl>
2019-05-11 11:43:14 +0200

    Scaffold

--- FridgePZ/Models/Categoryitempattern.cs
+++ FridgePZ/Models/Categoryitempattern.cs
@@ -12,12 +12,14 @@ namespace FridgePZ.Models
 
         public int CategoryItemPatternId { get; set; }
         public int StorageId { get; set; }
+        public int? NotificationId { get; set; }
         public string Name { get; set; }
         public string Type { get; set; }
         public decimal CurrentQuantity { get; set; }
         public decimal? ExpectedQuantity { get; set; }
         public decimal? ShopingFrequency { get; set; }
 
+        public virtual Notificationtype Notification { get; set; }
         public virtual Storage Storage { get; set; }
         public virtual ICollection<Itempattern> Itempattern { get; set; }
     }
--- FridgePZ/Models/Notificationtype.cs
+++ FridgePZ/Models/Notificationtype.cs
@@ -5,7 +5,16 @@ namespace FridgePZ.Models
 {
     public partial class Notificationtype
     {
+        public Notificationtype()
+        {
+            Categoryitempattern = new HashSet<Categoryitempattern>();
+            Item = new HashSet<Item>();
+        }
+
         public int NotificationId { get; set; }
         public string Type { get; set; }
+
+        public virtual ICollection<Categoryitempattern> Categoryitempattern { get; set; }
+        public virtual ICollection<Item> Item { get; set; }
     }
 }
